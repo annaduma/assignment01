17461e35e2cd5bbbbffb5cf300f7da66e4d2f31a
Anna Duma <aduma@uw.edu>
2017-06-17 19:02:44 -0700

    adds list element that prints array to DOM

--- array.js
+++ array.js
@@ -1,36 +1,26 @@
-
-
-// function submitButton(){
-//   event.preventDefault();
-//   var tasksArray = document.getElementById("difficulty").value;
-//   for (var i=0; i<tasksArray.length; i++)
-//   console.log(tasksArray);
-// }
-
-// function submitButton(){
-//   event.preventDefault();
-//   tasks.push(document.getElementById("description").value);
-// }
-
 var tasks = [];
 
+var taskCount = 0;
 function submitButton(){
-  tasks.push(document.querySelector('input[type = "radio"]:checked').value);
+  if (event != null) {
+    event.preventDefault();
+  }
+  var checked = document.querySelector('input[type = "radio"]:checked')
+  if (checked == null) {
+    return -1
+  }
+  tasks.push(checked.value);
   tasks.push(document.getElementById("description").value);
   tasks.push(document.getElementById("difficulty").value);
-  event.preventDefault();
+  todoList()
+  taskCount++;
 }
 
 function todoList(){
-  var parent = document.getElementById("list")
-  var ul = document.createElement("ul")
-
-  for (var i=0; i<tasks.length; i++){
-    console.log(tasks)
-    var list = document.createElement("li")
-
-    ul.appendChild(list)
-
-    parent.appendChild(ul)
-  }
+  var tasksArray = tasks
+  var div = document.getElementById("list")
+  var list = document.createElement("li");
+  var text = tasksArray[taskCount * 3] + " " + tasksArray[taskCount * 3 + 1] + " " + tasksArray[taskCount * 3 + 2];
+  list.innerHTML = text
+  div.appendChild(list)
 }
