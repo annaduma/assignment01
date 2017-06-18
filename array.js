19325c2fd4a8c6ddcba6711cf227257cd3209076
Anna Duma <aduma@uw.edu>
2017-06-17 17:42:55 -0700

    adds function which adds objects to an array

--- array.js
+++ array.js
@@ -1,33 +1,36 @@
-// var tasks = document.getElementById('taskDescription')
-//
-// window.onload = function(){
-//   var divElement = document.createElement("div")
-//   var ul = document.createElement("ul")
-//
-//   for (var i=0; i<tasks.length; i++){
-//   console.log(tasks[i])
-//   var li = document.createElement("li")
-//
-//   li.innerHTML = tasks[i]
-//
-//   ul.appendChild(li)
-//
-//   divElement.appendChild(ul)
-// }
+
+
+// function submitButton(){
+//   event.preventDefault();
+//   var tasksArray = document.getElementById("difficulty").value;
+//   for (var i=0; i<tasksArray.length; i++)
+//   console.log(tasksArray);
 // }
 
+// function submitButton(){
+//   event.preventDefault();
+//   tasks.push(document.getElementById("description").value);
+// }
 
 var tasks = [];
 
 function submitButton(){
-  event.preventDefault();
+  tasks.push(document.querySelector('input[type = "radio"]:checked').value);
   tasks.push(document.getElementById("description").value);
+  tasks.push(document.getElementById("difficulty").value);
+  event.preventDefault();
 }
 
-// tasks.push(document.getElementById("description"));
+function todoList(){
+  var parent = document.getElementById("list")
+  var ul = document.createElement("ul")
 
+  for (var i=0; i<tasks.length; i++){
+    console.log(tasks)
+    var list = document.createElement("li")
 
-// function todoList(){
-//   tasks.push(document.getElementById("difficulty"));
-//   console.log(tasks);
-// }
+    ul.appendChild(list)
+
+    parent.appendChild(ul)
+  }
+}
